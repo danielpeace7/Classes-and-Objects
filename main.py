3daff02ede61a1a4610f0f4bf8214ce8d0a815d7
Olayanju Daniel <102909682+danielpeace7@users.noreply.github.com>
2022-05-29 20:22:49 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,34 +1,28 @@
-from curses import ACS_GEQUAL
-from unicodedata import name
+# from curses import ACS_GEQUAL
+# from unicodedata import name
 
 
 class Student:
     # [assignment] Skeleton class. Add your code here
-    def __init__(self, name, age, tracks=[], score):
+    def __init__(self, name, age, score, tracks=[]):
         self.name = name
         self.age = age
-        self.tracks = tracks
         self.score = score
-
-    def changeName (self, NewName):
+        self.tracks = tracks
+    def change_name (self, NewName):
         self.name = NewName
-
-    def changeAge (self, NewAge):
+    def change_age (self, NewAge):
         self.age = int(NewAge)
-
-    def addTrack (self, NewTrack):
-        self.tracks.append(NewTrack)
-
-    def getScore (self):
+    def get_score (self):
         return self.score
-
+    def add_track (self, NewTrack):
+        self.tracks.append(NewTrack)
     def __repr__ (self):
-         return f'name: {self.name} age: {self.age} tracks: {self.tracks} score: {self.score}
-
+         return f"name: {self.name} age: {self.age} score: {self.score} tracks: {self.tracks}"
 Bob = Student(name="Bob", age=26, tracks=["FE","BE"],score=20.90)
-
 # Expected methods
 Bob.change_name("Peter")
 Bob.change_age(34)
 Bob.add_track("UI/UX")
 Bob.get_score()
+print(Bob)
